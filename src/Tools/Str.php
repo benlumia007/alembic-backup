f9f918f7171be32dbe0a7b4364a1be8a80d4548e
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-12-14 14:15:43 -0800

    updated

---
+++ src/Tools/Str.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Benlumia007\Alembic\Tools;
+
+class Str {
+
+	public static function slashBefore( $str ) {
+		return '/' . ltrim( $str, '/ ');
+	}
+
+	public static function slashAfter( $str ) {
+		return rtrim( $str, '/' ) . '/';
+	} 
+
+	public static function slashTrim( $str ) {
+		return trim( $str, '/' );
+	}
+
+	public static function startsWith( $str, $starts ) {
+		return substr( $str, 0, strlen( $starts ) ) === $starts;
+	}
+}

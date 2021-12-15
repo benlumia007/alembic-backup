a0b1c5a46be9afce7993323ff076eb2b3b2d1295
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-12-14 16:49:55 -0800

    updated

--- src/functions-helpers.php
+++ src/functions-helpers.php
@@ -108,21 +108,6 @@ function posts_per_page() {
 	return 10;
 }
 
-function cache_get( $name, $path = '' ) {
-
-	return cache( $name, $path )->get()->all();
-}
-
-function cache_set( $name, $path = '', $data = [] ) {
-
-	cache( $name, $path )->set( $data );
-}
-
-function cache_delete( $name, $path = '' ) {
-
-	cache( $name, $path )->delete();
-}
-
 function head_title() {
 	$route = Benlumia007\Alembic\App::resolve( 'routes' )->currentRoute();
 	$paged = '';

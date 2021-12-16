e8e40125e8ef1efb7e73c17940ba2f9b03e61bb9
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-12-16 08:58:49 -0800

    updated

--- src/Cache/functions-cache.php
+++ src/Cache/functions-cache.php
@@ -4,3 +4,16 @@ function cache_get( $name, $path = '' ) {
 
 	return cache( $name, $path )->get()->all();
 }
+
+function cache( $name, $path = '' ) {
+
+	$cache = Benlumia007\Alembic\App::resolve( 'cache' );
+
+	if ( $cache->has( $name ) ) {
+		return $cache->get( $name );
+	}
+
+	$cache->add( $name, new Benlumia007\Alembic\Cache\Component( $name, $path ) );
+
+	return $cache->get( $name );
+}

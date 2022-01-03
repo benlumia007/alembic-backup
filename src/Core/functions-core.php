4d33e8f3d45db09e3786edabf73737f076b5b3d8
benlumia007 <benlumia007@users.noreply.github.com>
2022-01-03 09:37:09 -0800

    update with app_archives()

--- src/Core/functions-core.php
+++ src/Core/functions-core.php
@@ -38,3 +38,50 @@ function app_categories() { ?>
 		<?php endforeach; ?>
 	</div>
 <?php }
+
+function app_archives() { ?>
+	<div class="archives">
+		<?php $current_year = $current_month = $current_day = '';
+			$posts = new Benlumia007\Alembic\ContentTypes\Entry\Entries( new Benlumia007\Alembic\ContentTypes\Entry\Locator( Benlumia007\Alembic\ContentTypes::get( 'post' )->path() ),
+				[
+					'order' => 'desc',
+					'number' => PHP_INT_MAX
+				] 
+			);
+		?>
+		<h2 class="archives-title">Archives</h2>
+
+		<div class="o-content-width mt-8">
+
+		<?php foreach ( $posts->all() as $post ) : ?>
+
+		<?php
+		$timestamp = is_numeric( $post->meta( 'date' ) ) ? $post->meta( 'date' ) : strtotime( $post->meta( 'date' ) );
+
+		// Get the post's year and month. We need this to compare it with the previous post date.
+		$year   = date( 'Y', $timestamp );
+		$month  = date( 'm', $timestamp );
+		$daynum = date( 'd', $timestamp );
+
+		// If the current date doesn't match this post's date, we need extra formatting.
+		if ( $current_year !== $year || $current_month !== $month ) :
+
+		// Set the current year and month to this post's year and month.
+		$current_year  = $year;
+		$current_month = $month;
+		$current_day   = '';
+
+		echo '<ul>';
+		printf(
+		'<li><a class="text-gray-700 no-underline hover:underline border-0" href="%s">%s</a></li>',
+		\Benlumia007\Alembic\App::resolve( 'content/types' )->get( 'post' )->uri( "{$year}/{$month}" ),
+		date( 'F Y', $timestamp )
+		);
+		echo '</ul>';
+
+
+		endif;
+		endforeach
+		?>
+	</div>
+<?php }

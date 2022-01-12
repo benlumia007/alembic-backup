1780448696363e2ddf4f8629daa36ed24c6c88ff
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-01-12 13:34:31 -0800

    updated

--- src/Entry/Types/Page.php
+++ src/Entry/Types/Page.php
@@ -5,6 +5,7 @@ namespace Benlumia007\Alembic\Entry\Types;
 use Benlumia007\Alembic\ContentTypes\Controllers\Page as Controller;
 use Benlumia007\Alembic\Routing\Routes\Component;
 use Benlumia007\Alembic\App;
+use Benlumia007\Alembic\Entry\Type;
 
 class Page extends Type {
 

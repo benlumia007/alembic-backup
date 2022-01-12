d426f5c6c259794306ff240327e88ad58abeb061
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-01-12 14:54:36 -0800

    updated

---
+++ src/Entry/Provider.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * View service provider.
+ *
+ * This is the service provider for the view system. The primary purpose of
+ * this is to use the container as a factory for creating views. By adding this
+ * to the container, it also allows the view implementation to be overwritten.
+ * That way, any custom functions will utilize the new class.
+ *
+ * @package   Alembic Core
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Benlumia007\Alembic\Entry;
+use Benlumia007\Alembic\Tools\ServiceProvider;
+use Benlumia007\Alembic\Entry\Types;
+use Benlumia007\Alembic\Proxies\ContentTypes;
+
+/**
+ * View provider class.
+ *
+ * @since  3.0.0
+ * @access public
+ */
+class Provider extends ServiceProvider {
+
+	/**
+	 * Binds the implementation of the view contract to the container.
+	 *
+	 * @since  3.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function register() {
+
+		// Bind a single instance of the engine contract.
+		$this->app->singleton( 'content/types', function() {
+            return new Types;
+        } );
+
+        $this->app->proxy( ContentTypes::class, 'Benlumia007\Alembic\ContentTypes' );
+	}
+}

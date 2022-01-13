d7d07f513948dd22586aa72771cd30b04970a978
benlumia007 <benlumia007@users.noreply.github.com>
2022-01-13 13:15:09 -0800

    update

--- src/Entry/Controllers/Error404.php
+++ src/Entry/Controllers/Error404.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace Benlumia007\Alembic\Entry\Controller;
-
-use Benlumia007\Alembic\Entry\Entries;
-use Benlumia007\Alembic\Entry\Locator;
-use Benlumia007\Alembic\Engine;
-
+namespace Benlumia007\Alembic\Entry\Controllers;
 use Benlumia007\Alembic\App;
+use Benlumia007\Alembic\Engine;
+use Benlumia007\Alembic\Routing\Entry\Entries;
+use Benlumia007\Alembic\Routing\Entry\Locator;
 
 class Error404 {
 

17bb81131f6eb7b2921bcf776f97454d35ffaa93
benlumia007 <benlumia007@users.noreply.github.com>
2022-01-17 20:45:27 -0800

    add author for controller or type

---
+++ src/Entry/Controllers/Author.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Benlumia007\Alembic\Entry\Controllers;
+
+use Benlumia007\Alembic\Routing\Entry\Entries;
+use Benlumia007\Alembic\Routing\Entry\Locator;
+use Benlumia007\Alembic\Engine;
+use Benlumia007\Alembic\ContentTypes;
+
+class Author {
+
+	protected $params = [];
+
+	public function __invoke( array $params = [] ) {
+
+		$this->params = (array) $params;
+
+		$this->slug = $this->params['name'];
+
+		$path    = ContentTypes::get( 'author' )->path();
+		$locator = new Locator( $path );
+		$terms = ( new Entries( $locator, [ 'slug' => $this->slug ] ) )->all();
+
+		Engine::display( 'author', [], [
+			'page'    => 1,
+			'entries' => $this->entries(),
+			'query'   => array_shift( $terms )
+		] );
+	}
+
+	protected function entries() {
+
+		$path = ContentTypes::get( 'post' )->path();
+
+		$locator = new Locator( $path );
+
+		$per_page = PHP_INT_MAX;
+		$current = isset( $this->params['number'] ) ? intval( $this->params['number'] ) : 1;
+
+		$args = [
+			'number'     => $per_page,
+			'offset'     => $per_page * ( $current - 1 ),
+			'order'      => 'desc',
+			'meta_key'   => 'author',
+			'meta_value' => sanitize_with_dashes( $this->params['name'] )
+		];
+
+		return new Entries( $locator, $args );
+	}
+}

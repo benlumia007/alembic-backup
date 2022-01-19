e0a2c86567ae5e33319566c07321cf405db7c5c3
benlumia007 <benlumia007@users.noreply.github.com>
2022-01-19 15:33:12 -0800

    update

---
+++ src/framework.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Framework instance
+ * 
+ * This file is used to create a new framework instance and adds specific features to the theme.
+ * 
+ * @package   Luthemes
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2014-2022. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Create a new framework instance.
+ */
+$alembic = new Benlumia007\Alembic\Core\Framework();
+
+/**
+ * Add default content types.
+ */
+Benlumia007\Alembic\ContentTypes::add( 'page', new Benlumia007\Alembic\Entry\Types\Page( $alembic->routes ) );
+Benlumia007\Alembic\ContentTypes::registerRoutes();
+
+// Launch the current controller.
+$current = $alembic->routes->current();

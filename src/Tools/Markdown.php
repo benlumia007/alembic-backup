baf72180d22a9578b23c6c87df34b5af3872f30b
benlumia007 <benlumia007@users.noreply.github.com>
2022-01-18 21:06:27 -0800

    update

--- src/Tools/Markdown.php
+++ src/Tools/Markdown.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Markdown
+ *
+ * @package   Alembic
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright 2021. Benjamin Lu
+ * @link      https://github.com/benlumia007/alembic
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
 
 namespace Benlumia007\Alembic\Tools;
 use ParsedownExtra;

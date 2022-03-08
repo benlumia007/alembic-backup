c42153e9c204091d8a7bd3883c0e074f042956b5
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-08 09:53:29 -0800

    udpated

--- src/Cache/functions-cache.php
+++ src/Cache/functions-cache.php
@@ -18,6 +18,22 @@ function cache() {
 	return Benlumia007\Alembic\Proxies\App::resolve( 'cache' );
 }
 
+function cache_add( string $name, string $type = 'collection' ) {
+	$cache = cache();
+
+	$map = [
+		'collection' => Cache\Collection::class,
+		'html'       => Cache\Html::class,
+		'json'       => Cache\Json::class,
+		'rapid'      => Cache\Rapid::class
+	];
+
+	if ( isset( $map[ $type ] ) ) {
+		$callback = $map[ $type ];
+		$cache->add( $name, new $callback( $name ) );
+	}
+}
+
 function cache_get( $name, $path = '' ) {
 
 	return cache( $name, $path )->get()->all();

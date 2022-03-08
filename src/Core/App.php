e106b2c1498b64b14ef4c2a7249b51a10a39154d
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-08 09:37:27 -0800

    udpated

--- src/Core/App.php
+++ src/Core/App.php
@@ -1,6 +1,7 @@
 <?php 
 
 namespace Benlumia007\Alembic\Core;
+use Benlumia007\Alembic\Tools\Collection;
 use Benlumia007\Alembic\Tools\ServiceProvider;
 
 class App extends ServiceProvider {
@@ -10,7 +11,7 @@ class App extends ServiceProvider {
         $this->app->instance( 'uri', $app[ 'uri' ] );
         $this->app->instance( 'uri/relative', $app[ 'uri' ], PHP_URL_PATH );
 
-        $this->app->instance( 'cache', new Benlumia007\Alembic\Tools\Collection() );
+        $this->app->instance( 'cache', new Collection() );
 
         $this->app->singleton( 'mix', function( $app ) {
 

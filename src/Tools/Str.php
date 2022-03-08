69bd81ba1cfced02e9f977beebea25aedf30a211
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-08 10:01:01 -0800

    ReADD STR

--- src/Tools/Str.php
+++ src/Tools/Str.php
@@ -1,30 +1,72 @@
 <?php
-/**
- * Str
- *
- * @package   Alembic
- * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright Copyright (C) 2019-2021. Benjamin Lu
- * @license   https://www.gnu.org/licenses/gpl-2.0.html
- */
 
 namespace Benlumia007\Alembic\Tools;
 
 class Str {
 
 	public static function slashBefore( $str ) {
-		return '/' . ltrim( $str, '/ ');
+
+		return '/' . ltrim( $str, '/' );
 	}
 
 	public static function slashAfter( $str ) {
+
 		return rtrim( $str, '/' ) . '/';
-	} 
+	}
 
 	public static function slashTrim( $str ) {
+
 		return trim( $str, '/' );
 	}
 
 	public static function startsWith( $str, $starts ) {
+
 		return substr( $str, 0, strlen( $starts ) ) === $starts;
+	}
+
+	public static function replaceLast( $search, $replace, $str ) {
+		if ( '' === $search ) {
+			return $str;
+		}
+
+		$pos = strpos( $str, $search );
+
+		if ( false !== $pos ) {
+			return substr_replace( $str, $replace, $pos, strlen( $search ) );
+		}
+
+		return $str;
+	}
+
+	public static function substr( $str, $start, $length = null ) {
+		return mb_substr( $str, $start, $length, 'UTF-8' );
+	}
+
+	public static function beforeLast( $str, $search ) {
+		if ( '' === $search ) {
+			return $str;
+		}
+
+		$pos = mb_strrpos( $str, $search );
+
+		if ( false === $pos ) {
+			return $str;
+		}
+
+		return static::substr( $str, 0, $pos );
+	}
+
+	public static function afterLast( $str, $search ) {
+		if ( '' === $search ) {
+			return $str;
+		}
+
+		$pos = strrpos( $str, $search );
+
+		if ( false === $pos ) {
+			return $str;
+		}
+
+		return substr( $str, $pos + strlen( $search ) );
 	}
 }

aaa872504f13c67836e984799c98a8046c059fac
benlumia007 <benlumia007@users.noreply.github.com>
2022-03-07 22:04:44 -0800

    update

--- src/Framework.php
+++ src/Framework.php
@@ -117,6 +117,8 @@ class Framework extends Container implements Application, Bootable {
 
 		// Add the version for the framework.
 		$this->instance( 'version', static::VERSION );
+
+		$this->instance( 'path/content', 'user/content')
 	}
 
 	/**

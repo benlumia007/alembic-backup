26e47423475ef6845f52a46a469208c0e7eb4481
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-08 14:27:53 -0800

    udpated

---
+++ src/Controllers/Error404.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 404 controller.
+ *
+ * @package   Blush
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2018 - 2022, Justin Tadlock
+ * @link      https://github.com/blush-dev/framework
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+namespace Benlumia007\Alembic\Controllers;
+
+use Benlumia007\Alembic\Proxies\App;
+use Benlumia007\Alembic\Content\Query;
+
+class Error404 extends Controller {
+
+	public function __invoke( array $params = [] ) {
+		$this->params = $params;
+
+		http_response_code( 404 );
+
+		$entries = new Query( '_error', [ 'slug' => '404' ] );
+
+		$all   = $entries->all();
+		$entry = array_shift( $all );
+
+		return $this->response(
+			$this->view( [
+				'error-404',
+				'error'
+			], [
+				'title'   => $entry ? $entry->title() : 'Not Found',
+				'query'   => $entry ? $entry : false,
+				'page'    => 1,
+				'entries' => $entries
+			] )
+		);
+	}
+}

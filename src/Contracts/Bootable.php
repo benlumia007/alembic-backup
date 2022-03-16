c3d3fede993a16ea875bb9514c6b81a8bac7e24c
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-15 20:44:21 -0700

    add src/Contracts/Bootable

---
+++ src/Contracts/Bootable.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Bootable interface.
+ *
+ * Defines the contract that bootable classes should utilize. Bootable classes
+ * should have a `boot()` method with the singular purpose of "booting" the any
+ * necessary code that needs to run. Most bootable classes are meant to be
+ * single-instance classes that get loaded once per page request.
+ *
+ * @package   Alembic
+ * @author    Benjamin Lu <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2019 - 2022, Benjamin Lu
+ * @link      https://github.com/benlumia007/alembic
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+namespace Blush\Contracts;
+
+interface Bootable
+{
+	/**
+	 * Bootstraps the class.
+	 *
+	 * @since 1.0.0
+	 */
+	public function boot(): void;
+}

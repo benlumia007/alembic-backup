d9f7fc7db32023e33b0d827896916fb63f2ff61a
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-15 20:47:49 -0700

    add src/Contracts/Renderable

---
+++ src/Contracts/Renderable.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Renderable contract.
+ *
+ * Renderable classes should implement a `render()` method that returns an HTML
+ * string ready for output to the screen. While there's no way to ensure this
+ * via the contract, the intent here is for anything that's renderable to already
+ * be escaped. For clarity in the code, when returning raw data, it is
+ * recommended to use an alternate method name, such as `get()`, and not use
+ * this contract.
+ *
+ * @package   Alembic
+ * @author    Benjamin Lu <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2019 - 2022, Benjamin Lu
+ * @link      https://github.com/benlumia007/alembic
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+namespace Blush\Contracts;
+
+interface Renderable
+{
+	/**
+	 * Returns an HTML string for output.
+	 *
+	 * @since 1.0.0
+	 */
+	public function render(): string;
+}

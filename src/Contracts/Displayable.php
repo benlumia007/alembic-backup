7fae39ea30abe9d0d6b6eb5e830441b7839430d7
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-15 20:45:28 -0700

    add src/Contracts/Displayable

---
+++ src/Contracts/Displayable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Displayable contract.
+ *
+ * Displayable classes should implement a `display()` method. The intent of this
+ * method is to output an HTML string to the screen. This data should already be
+ * escaped prior to being output.
+ *
+ * @package   Alembic
+ * @author    Benjamin Lu <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2019 - 2022, Benjamin Lu
+ * @link      https://github.com/benlumia007/alembic
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+namespace Blush\Contracts;
+
+interface Displayable
+{
+	/**
+	 * Prints the HTML string.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function display();
+}

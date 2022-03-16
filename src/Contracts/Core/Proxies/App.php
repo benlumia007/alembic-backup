65f6c2394f3660881067bc641bcb847b5f4afbf8
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-15 21:13:32 -0700

    add src/Core/Proxies/App

---
+++ src/Contracts/Core/Proxies/App.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * App static proxy class.
+ *
+ * Static proxy class for the application instance.
+ *
+ * @package   Alembic
+ * @author    Benjamin Lu <benlumia007k@gmail.com>
+ * @copyright Copyright (c) 2019 - 2022, Justin Tadlock
+ * @link      https://github.com/blush-dev/framework
+ * @license   https://opensource.org/licenses/MIT
+ */
+
+namespace Benlumia007\Alembic\Core\Proxies;
+use Benlumia007\Alembic\Core\Proxy;
+
+class App extends Proxy
+{
+	/**
+	 * Returns the name of the accessor for object registered in the container.
+	 *
+	 * @since 1.0.0
+	 */
+	protected static function accessor(): string {
+		return 'app';
+	}
+}
